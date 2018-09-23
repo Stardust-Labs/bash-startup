bda196bd723c87c92cfbb0b9c9aff85ff680ee94
James Ekrut <jamekrut@gmail.com>
2018-09-23 15:47:40 +0000

    Moved bashfile creation to external file

---
+++ create-bashfiles.sh
@@ -0,0 +1,38 @@
+cat > .bash-aliases <<- EOM
+## convenience and shortcut aliases
+alias python=python3
+alias pip=pip3
+alias gimp="sommelier -X --scale=0.8 --dpi=160 gimp"
+
+## ssh aliases
+
+## directory shortcuts
+
+## silliness
+
+EOM
+
+cat > .bash-fucntions <<- EOM
+function mkdircd {
+  mkdir \$1
+  cd \$1
+}
+
+function quicksom {
+  sommelier -X --scale=\$1 --dpi=160 \$2
+}
+
+EOM
+
+cat > .bash-exports <<- EOM
+export PATH=~/devtools/flutter/bin:\$PATH
+
+EOM
+
+cat > .startup <<- EOM
+source ~/.bash-extensions/.bash-aliases
+source ~/.bash-extensions/.bash-functions
+source ~/.bash-extensions/.bash-exports
+
+EOM
+
--- crostini-start.sh
+++ crostini-start.sh
@@ -53,40 +53,5 @@ rm flutter.tar.xz
 
 ## create custom aliases and functions files
 cd ~/.bash-extensions
-
-cat > .bash-aliases <<- EOM
-## convenience and shortcut aliases
-alias python=python3
-alias pip=pip3
-alias gimp="sommelier -X --scale=0.8 --dpi=160 gimp"
-
-## ssh aliases
-
-## directory shortcuts
-
-## silliness
-
-EOM
-
-cat > .bash-functions <<- EOM
-function mkdircd {
-  mkdir \$1
-  cd \$1
-}
-
-function quicksom {
-  sommelier -X --scale=\$1 --dpi=160 \$2
-}
-
-EOM
-
-cat > .bash-exports <<- EOM
-export PATH=/home/jamekrut/devtools/Flutter/bin:$PATH
-EOM
-
-cat > .startup <<- EOM
-source ~/.bash-extensions/.bash-aliases
-source ~/.bash-extensions/.bash-functions
-source ~/.bash-extensions/.bash-exports
-EOM
+source create-bashfiles.sh
 
