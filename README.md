2c64d1dd64287b525e6f80d78ae45948951ca665
James Ekrut <jamekrut@gmail.com>
2018-09-23 15:09:34 +0000

    Created fresh Crostini installation startup script

---
+++ README.md
@@ -0,0 +1,14 @@
+In a new Chrome OS / Crostini install, installs a number of needed packages and their dependencies.  Major installs include:
+PHP 7.2
+Python 3
+Pip 3
+Composer
+Flutter
+GIMP
+Okular
+Sublime Text
+Sublime Merge
+
+Also creates .bash-aliases, .bash-functions, and .bash-exports with common config options.
+
+After running the script, .bash-extensions/.startup must be sourced in the local .bashrc file.
---
+++ crostini-start.sh
@@ -0,0 +1,92 @@
+## create folder architecture
+cd ~
+mkdir downloads
+mkdir devtools
+mkdir projects
+mkdir .ssh
+mkdir .bash-extensions
+
+## install basic things that are needed
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install wget curl man-db
+
+## install php7.2
+sudo apt-get -y install apt-transport-https lsb-release ca-certificates
+sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
+
+sudo apt-get update
+sudo apt-get -y install php7.2
+
+sudo apt-get -y install php7.2-cli php7.2-common php7.2-curl php7.2-gd php7.2-json php7.2-mbstring php7.2-mysql php7.2-opcache php7.2-readline php7.2-xml php7.2-zip
+
+## install composer globally
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+sudo mv composer.phar /usr/local/bin/composer
+
+## install python
+sudo apt-get -y install python3 python3-pip
+
+## install sublime text and merge
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get -y install sublime-text sublime-merge
+
+## install gimp
+sudo apt-get -y install gimp
+
+## install okular
+sudo apt-get -y install okular
+
+## install flutter globally
+cd devtools
+wget https://storage.googleapis.com/flutter_infra/releases/beta/linux/flutter_linux_v0.8.2-beta.tar.xz -O flutter.tar.xz
+tar xf flutter.tar.xz
+rm flutter.tar.xz
+#export PATH=~/devtools/flutter/bin:$PATH
+
+## create custom aliases and functions files
+cd ~/.bash-extensions
+
+cat > .bash-aliases <<- EOM
+## convenience and shortcut aliases
+alias python=python3
+alias pip=pip3
+alias gimp="sommelier -X --scale=0.8 --dpi=160 gimp
+
+## ssh aliases
+
+## directory shortcuts
+
+## silliness
+
+EOM
+
+cat > .bash-functions <<- EOM
+function mkdircd {
+  mkdir \$1
+  cd \$1
+}
+
+function quicksom {
+  sommelier -X --scale=\$1 --dpi=160 \$2
+}
+
+EOM
+
+cat > .bash-exports <<- EOM
+export PATH=/home/jamekrut/devtools/Flutter/bin:$PATH
+EOM
+
+cat > .startup <<- EOM
+source ~/.bash-extensions/.bash-aliases
+source ~/.bash-extensions/.bash-functions
+source ~/.bash-extensions/.bash-exports
+EOM
+
